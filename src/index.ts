b24416aef52540c96139ae22c03d35f799707464
Matheus Magno <matheusmagnon@gmail.com>
2023-08-23 21:34:12 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("test");
+import express from "express";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  res.send("run");
+});
+
+app.listen(port, () => console.log(`lintening port ${port}`));
